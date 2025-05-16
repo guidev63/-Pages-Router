ba9102c5f6c90b976a198a61075f21c95ccd4bf9
Guilherme Augusto <guilhermedev23@gmail.com>
2025-05-16 00:54:15 -0300

    ok

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('postcss-load-config').config} */
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
---
+++ src/pages/blog/user/index.tsx
@@ -0,0 +1,8 @@
+export default function  UserPage( ){
+  return (
+    <div
+    >
+      <h2>User</h2>
+    </div>
+  )
+}
